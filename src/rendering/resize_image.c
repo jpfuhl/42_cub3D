fabc99cf22886ff701d0ce06f19d2c80a822c26a
Joko Pfuhl <jpfuhl@3-B-1.42heilbronn.de>
2022-07-11 20:09:45 +0200

    new files, old files delete

---
+++ src/rendering/resize_image.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   resize_image.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpfuhl <jpfuhl@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/24 12:53:15 by jpfuhl            #+#    #+#             */
+/*   Updated: 2022/07/11 19:23:20 by jpfuhl           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/rendering.h"
+
+int	get_pixel_colour(t_image *image, int x, int y)
+{
+	char	*add;
+	int		ll;
+	int		bpp;
+	char	*dst;
+
+	add = image->address;
+	ll = image->line_length;
+	bpp = image->bits_per_pixel;
+	dst = add + (y * ll + x * (bpp / 8));
+	return (*(unsigned int *)dst);
+}
+
+int	get_pixel_from_source(t_image *dst, t_image *src, int x, int y)
+{
+	int		colour;
+	double	scale_x;
+	double	scale_y;
+
+	scale_x = x * (1.0 * src->width / dst->width);
+	scale_y = y * (1.0 * src->height / dst->height);
+	colour = get_pixel_colour(src, (int)scale_x, (int)scale_y);
+	return (colour);
+}
+
+void	resize_image(t_image *destination, t_image *source)
+{
+	int	x;
+	int	y;
+	int	colour;
+
+	y = 0;
+	while (y < destination->height)
+	{
+		x = 0;
+		while (x < destination->width)
+		{
+			colour = get_pixel_from_source(destination, source, x, y);
+			if (colour)
+				draw_pixel(destination, x, y, colour);
+			x++;
+		}
+		y++;
+	}
+}
